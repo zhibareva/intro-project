52193274cc750b91ca7814e98c6eabe670ab251c
Anastasia Zhibareva <zhibareva.a@gmail.com>
2022-12-26 10:23:46 +0300

    rest generated soap client

---
+++ Rest/src/main/java/org/example/soap/ws/client/generated/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://localhost:7071/ws", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package org.example.soap.ws.client.generated;
